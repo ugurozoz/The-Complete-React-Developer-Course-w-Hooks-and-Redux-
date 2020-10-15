5b3e471972fa900f611da4739d7b0077ff477a65
ugur ozoz <ugurozoz@yahoo.com>
2020-10-15 23:28:10 +0300

    Section 8:129

--- the-burger-builder/src/App.js
+++ the-burger-builder/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import BurgerIngredient from "./components/Burger/BurgerIngredient/BurgerIngredient"
+// import BurgerIngredient from "./components/Burger/BurgerIngredient/BurgerIngredient"
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <BurgerIngredient type="meat"/>
+          <BurgerBuilder />          
         </Layout>
       </div>
     );
