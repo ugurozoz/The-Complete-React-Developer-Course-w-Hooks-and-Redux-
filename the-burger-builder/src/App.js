6043cea7d591b948862084675774c8e33aa78aa6
ugur ozoz <ugurozoz@yahoo.com>
2020-10-15 21:50:34 +0300

    Section 8:126

--- the-burger-builder/src/App.js
+++ the-burger-builder/src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import BurgerIngredient from "./components/Burger/BurgerIngredient/BurgerIngredient"
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <BurgerBuilder />
+          <BurgerIngredient type="meat"/>
+        </Layout>
       </div>
     );
   }
---
+++ the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import Aux from "../../hoc/Auxiliary";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Burger</div>
+        <div>Burger</div>
+        <div>Burger</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
