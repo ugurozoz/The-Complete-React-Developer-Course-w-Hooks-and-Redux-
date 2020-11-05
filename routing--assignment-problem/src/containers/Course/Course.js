6336851ca6d890fff19a0bfacfe1e3a2692b890c
ugur ozoz <ugurozoz@yahoo.com>
2020-11-05 21:57:18 +0300

    Assignment 3 completed

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
+  state = {
+    title: null,
+  };
+
+  componentDidMount() {
+    console.log('DidMount Worked')
+    this.setState({ title: this.getTitleFromParams() });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate >>>', prevState)
+    if (prevState.title !== this.getTitleFromParams()) {
+      this.setState({ title: this.getTitleFromParams() });      
     }
+    
+  }
+
+  getTitleFromParams() {
+    const query = new URLSearchParams(this.props.location.search);
+    for (let param of query.entries()) {
+      let title = param[1];
+      return title;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>You selected the Course with ID: {this.props.match.params.id}</p>
+      </div>
+    );
+  }
 }
 
 export default Course;
--- routing--assignment-problem/src/containers/Courses/Courses.js
+++ routing--assignment-problem/src/containers/Courses/Courses.js
@@ -1,30 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Route,
+  NavLink,
+  Switch,
+  Redirect,
+  Link,
+} from "react-router-dom";
+import Course from "../Course/Course";
 
-import './Courses.css';
+import "./Courses.css";
 
 class Courses extends Component {
-    state = {
-        courses: [
-            { id: 1, title: 'Angular - The Complete Guide' },
-            { id: 2, title: 'Vue - The Complete Guide' },
-            { id: 3, title: 'PWA - The Complete Guide' }
-        ]
-    }
+  state = {
+    courses: [
+      { id: 1, title: "Angular - The Complete Guide" },
+      { id: 2, title: "Vue - The Complete Guide" },
+      { id: 3, title: "PWA - The Complete Guide" },
+    ],
+  };
 
-    render () {
-        return (
-            <div>
-                <h1>Amazing Udemy Courses</h1>
-                <section className="Courses">
-                    {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
-                    }
-                </section>
-            </div>
-        );
-    }
+  componentDidMount() {
+    //console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Amazing Udemy Courses</h1>
+        <section className="Courses">
+          {this.state.courses.map((course) => {
+            return (
+              <NavLink
+                key={course.id}
+                to={{
+                  pathname: `/courses/${course.id}`,
+                  search: `?title=${course.title}`
+                }}
+              >
+                <article className="Course" key={course.id}>
+                  {course.title}
+                </article>
+              </NavLink>
+            );
+          })}
+        </section>
+        <Route path={this.props.match.url + "/:id"} component={Course} />
+      </div>
+    );
+  }
 }
 
 export default Courses;
+// to={{
+//   pathname: "/new-post",
+//   hash: "#submit",
+//   search: "?quick-submit=true",
+// }}
