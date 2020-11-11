d0659dc624ec5241706b4f430b026007479e71b7
ugur ozoz <ugurozoz@yahoo.com>
2020-11-11 20:42:14 +0300

    Section 14:251-262

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -28,14 +30,40 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)}  />
+                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)}  />
             </div>
         );
     }
 }
 
-export default Counter;
+
+// Connect local ctr to store counter
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncrementCounter: () => dispatch({
+      type: 'INCREMENT'
+    }),
+    onDecrementCounter: () => dispatch({
+      type: 'DECREMENT'
+    }),
+    onAddCounter: (val) => dispatch({
+      type: 'ADD', value: val
+    }),
+    onSubtractCounter: (val) => dispatch({
+      type: 'SUBTRACT', value: val
+    })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+//export default Counter;
