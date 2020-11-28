98a490f88250925b399846ceb5e6fb0541631ce3
ugur ozoz <ugurozoz@yahoo.com>
2020-11-28 22:51:07 +0300

    Section 22 / css&style loader installed and configured

--- webpack-prj/webpack.config.js
+++ webpack-prj/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
   mode: 'development',
@@ -15,6 +16,27 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+              },
+            },
+          }, {
+            loader: 'postcss-loader', options: {
+              ident: 'postcss',
+              plugins: () => [autoprefixer()]
+            }
+          },
+        ],
       },
     ],
   },
